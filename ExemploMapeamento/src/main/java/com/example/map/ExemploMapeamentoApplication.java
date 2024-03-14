dbaf322a84647afdd5500f741159852c74476a64
yDavidd <132359951+yDavidd@users.noreply.github.com>
2024-03-14 14:39:30 -0300

    Add files via upload

---
+++ ExemploMapeamento/src/main/java/com/example/map/ExemploMapeamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMapeamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMapeamentoApplication.class, args);
+	}
+
+}
